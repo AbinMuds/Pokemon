a536a10ab87f0412e0df48d96fb9a30d380a0bed
ABHI <am@MacBook-Pro.local>
2021-02-01 11:35:45 -0800

    made a list of pokemons in First page grabing from Poke Api and using for loop to make div element of every pokemon and its image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log("sanity check")
+form = document.createElement("FORM")
+text = document.createElement("h1")
+
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((response) => response.json())
+    .then((data) => {
+        let pokemonList = []
+        pokemonList = data.results
+        for(let i=0;i<pokemonList.length;i++){
+            div = document.createElement("div")
+            // console.log(pokemonList[i])
+            div.innerHTML = pokemonList[i].name
+            document.body.appendChild(div)
+
+            function createPokeImage(pokeID, containerDiv){
+                let pokeImage = document.createElement('img')
+                pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
+                containerDiv.append(pokeImage);
+              }
+
+            createPokeImage(i+1,div)
+        }
+        
+    })
+
+
