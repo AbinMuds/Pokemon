539cf631f6ccfd82db6af41825cc3b0faf03aada
ABHI <am@MacBook-Pro.local>
2021-02-01 20:14:40 -0800

    css html and js files created with some code for  functionality

--- js/pokedex.js
+++ js/pokedex.js
@@ -5,42 +5,29 @@ console.log("sanity check")
 mainDiv = document.createElement("div")
 document.body.appendChild(mainDiv)
 mainDiv.setAttribute("id","container")
-fetch("https://pokeapi.co/api/v2/pokemon?limit=200")
+fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((response) => response.json())
     .then((data) => {
         let pokemonList = []
         pokemonList = data.results
         for(let i=0;i<pokemonList.length;i++){
-
-            div = document.createElement("div")
-            mainDiv.appendChild(div)
-            div.setAttribute("id",i)
-            // text = document.createElement("h1")
-            // list.appendChild(text)
-            div.innerHTML = pokemonList[i].name
-            // list = document.createElement("li")
-            // div.append(list)
             function createPokeImage(pokeID, containerDiv){
                 let pokeImage = document.createElement('img')
                 pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
                 containerDiv.append(pokeImage);
               }
-            createPokeImage(i+1,div)
-            // getTypeof(i+1,div)
-            // console.log(pokemonList[i])
+            // createPokeImage(i+1,div)
             btn = document.createElement("button")
             btn.textContent = "Pokedex"
             div.appendChild(btn)
             btn.setAttribute("id","bttn"+i) 
-            document.getElementById("bttn"+i).addEventListener("click",function() {
-                // text = document.createElement('h1')
-                // document.body.appendChild(text)
-                // text.textContent = pokemonList[i] 
-                location.href="../html/Pokedex.html"
-                // console.log("hello")
-            })
-        
+            document.getElementById("bttn").addEventListener("click",function() {
+                text = document.createElement('h1')
+                document.body.appendChild(text)
+                this.textContent = pokemonList[i].name 
         }
+        
+        })
         function getTypeof(pokeID, containerDiv){
             fetch("https://pokeapi.co/api/v2/pokemon/2")
             .then((response) => response.json())
